09ed91ab33d278287f8f4e18280fa70e465aa7b2
zhangqiao <1226937462@qq.com>
2023-05-29 09:08:48 +0800

    vue flex基础样式添加

---
+++ README.md
@@ -0,0 +1,11 @@
+flexible rem适配不同端；
+
+postcss-pxtorem 编码px转rem; 
+
+适用大屏 和 各种设计图产品
+
+element-ui 
+
+vuex,vue-router,sass,sass-loader
+
+
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
-import './common/flexible'
+import './common/js/flexible'
 
 Vue.config.productionTip = false
 
